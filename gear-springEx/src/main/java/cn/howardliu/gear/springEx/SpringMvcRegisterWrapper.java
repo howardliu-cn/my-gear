ad306f7ab44a0c3ffcb4807bd1f44d980f77297c
howardliu-cn <liuxinghao1988@gmail.com>
2017-03-16 15:54:44 +0800

    注册包装类增加port参数

--- gear-springEx/src/main/java/cn/howardliu/gear/springEx/SpringMvcRegisterWrapper.java
+++ gear-springEx/src/main/java/cn/howardliu/gear/springEx/SpringMvcRegisterWrapper.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.io.IOException;
 
@@ -29,10 +28,14 @@ public class SpringMvcRegisterWrapper implements ISpringMvcRegister {
     private CuratorFramework client;
     private String basePath;
     private String preServiceName = "";
+    private String localIp = null;
+    private Integer localPort = null;
 
     @Override
-    @PostConstruct
     public void regist() throws Exception {
+        if (this.springMvcRegister != null) {
+            this.springMvcRegister.close();
+        }
         this.springMvcRegister = new SpringMvcRegister(this.client, this.basePath, this.getLocalIp(), this.getPort());
         this.springMvcRegister.setPreServiceName(this.preServiceName);
         this.springMvcRegister.setSpringMvcServiceScanner(this.springMvcServiceScanner);
@@ -42,7 +45,9 @@ public class SpringMvcRegisterWrapper implements ISpringMvcRegister {
     @Override
     @Scheduled(initialDelay = 10000, fixedDelay = 30000)
     public void refresh() throws Exception {
-        this.springMvcRegister.refresh();
+        if (springMvcRegister != null) {
+            this.springMvcRegister.refresh();
+        }
     }
 
     @Override
@@ -52,8 +57,11 @@ public class SpringMvcRegisterWrapper implements ISpringMvcRegister {
     }
 
     public String getLocalIp() {
+        if (localIp != null) {
+            return localIp;
+        }
         String ipEnvName = Validate.notNull(this.getIpEnvName(), "本地IP地址环境变量[默认为" + DEFAULT_IP_ENV_NAME + "]不能为空，请检查！");
-        String localIp = System.getenv(ipEnvName);
+        localIp = System.getenv(ipEnvName);
         if (StringUtils.isBlank(localIp)) {
             localIp = System.getProperty(ipEnvName);
         }
@@ -61,8 +69,12 @@ public class SpringMvcRegisterWrapper implements ISpringMvcRegister {
     }
 
     public Integer getPort() throws Exception {
+        if (localPort != null) {
+            return localPort;
+        }
         // TODO 需要根据容器进行判断
-        return TomcatInfoUtils.getPort();
+        localPort = TomcatInfoUtils.getPort();
+        return Validate.notNull(localPort, "服务端口不能为空，请检查！");
     }
 
     public String getIpEnvName() {
@@ -90,5 +102,13 @@ public class SpringMvcRegisterWrapper implements ISpringMvcRegister {
 
     public void setPreServiceName(String preServiceName) {
         this.preServiceName = preServiceName;
+    }
+
+    public void setLocalIp(String localIp) {
+        this.localIp = localIp;
+    }
+
+    public void setLocalPort(Integer localPort) {
+        this.localPort = localPort;
     }
 }

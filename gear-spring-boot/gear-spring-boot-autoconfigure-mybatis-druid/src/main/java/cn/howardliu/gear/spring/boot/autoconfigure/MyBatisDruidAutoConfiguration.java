f18a58143e404a0dba70d8940f4fbd98dab7120e
howardliu-cn <liuxinghao1988@gmail.com>
2017-05-16 17:09:09 +0800

    spring boot starter: mybatis integrate with druid

---
+++ gear-spring-boot/gear-spring-boot-autoconfigure-mybatis-druid/src/main/java/cn/howardliu/gear/spring/boot/autoconfigure/MyBatisDruidAutoConfiguration.java
@@ -0,0 +1,101 @@
+package cn.howardliu.gear.spring.boot.autoconfigure;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * <p>
+ * {@link org.springframework.boot.autoconfigure.EnableAutoConfiguration Auto-Configuration} for Mybatis integrate with Druid.
+ * <p>
+ * Contributes:
+ * <ul>
+ * <li>{@link DruidDataSource}</li>
+ * <li>{@link org.springframework.jdbc.datasource.DataSourceTransactionManager}</li>
+ * </ul>
+ * <p>
+ * If {@link org.mybatis.spring.annotation.MapperScan} is used, or a
+ * configuration file is specified as a property, those will be considered,
+ * otherwise this auto-configuration will attempt to register mappers based on
+ * the interface definitions in or under the root auto-configuration package.
+ *
+ * @author liuxh
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Configuration
+@ConditionalOnClass(DataSource.class)
+@ConditionalOnProperty(name = "spring.datasource.type", havingValue = "com.alibaba.druid.pool.DruidDataSource", matchIfMissing = true)
+@EnableConfigurationProperties(MybatisDruidProperties.class)
+@AutoConfigureBefore({MybatisAutoConfiguration.class, DataSourceAutoConfiguration.class})
+public class MyBatisDruidAutoConfiguration {
+    private static final Logger logger = LoggerFactory.getLogger(MyBatisDruidAutoConfiguration.class);
+    private final MybatisDruidProperties properties;
+
+    public MyBatisDruidAutoConfiguration(MybatisDruidProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean(initMethod = "init", destroyMethod = "close")
+    @ConditionalOnMissingBean
+    public DataSource dataSource() throws SQLException {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(this.properties.getDriverClassName());
+        dataSource.setUrl(this.properties.getJdbcUrl());
+        dataSource.setUsername(this.properties.getUsername());
+        dataSource.setPassword(this.properties.getPassword());
+        dataSource.setInitialSize(this.properties.getInitialPoolSize());
+        dataSource.setMinIdle(this.properties.getMinPoolSize());
+        dataSource.setMaxActive(this.properties.getMaxPoolWait());
+        dataSource.setMaxWait(60000);
+        dataSource.setTimeBetweenEvictionRunsMillis(6000);
+        dataSource.setMinEvictableIdleTimeMillis(300000);
+        dataSource.setValidationQuery("SELECT 'x'");
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnBorrow(false);
+        dataSource.setTestOnReturn(false);
+        dataSource.setPoolPreparedStatements(true);
+        dataSource.setMaxPoolPreparedStatementPerConnectionSize(20);
+        dataSource.setFilters("stat");
+        return dataSource;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public DataSourceTransactionManager transactionManager(DataSource dataSource) throws SQLException {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Configuration
+    @ConditionalOnMissingClass
+    public static class DruidDataSourceNotFoundConfiguration {
+        @PostConstruct
+        public void afterPropertiesSet() {
+            logger.debug("No {} found.", DruidDataSource.class.getName());
+        }
+    }
+
+    @Configuration
+    @ConditionalOnMissingClass("com.alibaba.druid.pool.DruidDataSource")
+    public static class DruidDataSourceNotCreateConfiguration {
+        @PostConstruct
+        public void afterPropertiesSet() {
+            logger.debug("No {} bean create.", DruidDataSource.class.getName());
+        }
+    }
+}
---
+++ gear-spring-boot/gear-spring-boot-autoconfigure-mybatis-druid/src/main/java/cn/howardliu/gear/spring/boot/autoconfigure/MybatisDruidProperties.java
@@ -0,0 +1,89 @@
+package cn.howardliu.gear.spring.boot.autoconfigure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import static cn.howardliu.gear.spring.boot.autoconfigure.MybatisDruidProperties.MYBATIS_DRUID_PREFIX;
+
+/**
+ * <br>created at 17-5-16
+ *
+ * @author liuxh
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@ConfigurationProperties(prefix = MYBATIS_DRUID_PREFIX)
+public class MybatisDruidProperties {
+    @SuppressWarnings("WeakerAccess")
+    public static final String MYBATIS_DRUID_PREFIX = "mybatis.druid";
+    private String driverClassName;
+    private String jdbcUrl;
+    private String username;
+    private String password;
+    private int initialPoolSize;
+    private int minPoolSize;
+    private int maxPoolWait;
+    // TODO need to complete all field for druid
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public MybatisDruidProperties setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+        return this;
+    }
+
+    public String getJdbcUrl() {
+        return jdbcUrl;
+    }
+
+    public MybatisDruidProperties setJdbcUrl(String jdbcUrl) {
+        this.jdbcUrl = jdbcUrl;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public MybatisDruidProperties setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public MybatisDruidProperties setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public int getInitialPoolSize() {
+        return initialPoolSize;
+    }
+
+    public MybatisDruidProperties setInitialPoolSize(int initialPoolSize) {
+        this.initialPoolSize = initialPoolSize;
+        return this;
+    }
+
+    public int getMinPoolSize() {
+        return minPoolSize;
+    }
+
+    public MybatisDruidProperties setMinPoolSize(int minPoolSize) {
+        this.minPoolSize = minPoolSize;
+        return this;
+    }
+
+    public int getMaxPoolWait() {
+        return maxPoolWait;
+    }
+
+    public MybatisDruidProperties setMaxPoolWait(int maxPoolWait) {
+        this.maxPoolWait = maxPoolWait;
+        return this;
+    }
+}

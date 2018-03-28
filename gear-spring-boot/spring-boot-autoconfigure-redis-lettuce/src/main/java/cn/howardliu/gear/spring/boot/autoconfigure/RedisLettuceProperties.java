80677fef3df6391db76eb5620656d4fe2e690d7a
howardliu-cn <liuxinghao1988@gmail.com>
2018-03-28 15:00:59 +0800

    lettuce properties in spring boot 2.0.0

--- gear-spring-boot/spring-boot-autoconfigure-redis-lettuce/src/main/java/cn/howardliu/gear/spring/boot/autoconfigure/RedisLettuceProperties.java
+++ gear-spring-boot/spring-boot-autoconfigure-redis-lettuce/src/main/java/cn/howardliu/gear/spring/boot/autoconfigure/RedisLettuceProperties.java
@@ -14,65 +14,13 @@ import static cn.howardliu.gear.spring.boot.autoconfigure.RedisLettuceProperties
 @ConfigurationProperties(prefix = REDIS_LETTUCE_PREFIX)
 public class RedisLettuceProperties {
     public static final String REDIS_LETTUCE_PREFIX = "spring.redis.cluster";
+    private String prefix;
 
-    private String nodes;
-    private int maxRedirects = 3;
-    private String password;
-    private boolean loadRemoteCachesOnStartup = true;
-    private boolean usePrefix = true;
-    private int defaultExpiration = 180;
-
-    public String getNodes() {
-        return nodes;
+    public String getPrefix() {
+        return prefix;
     }
 
-    public RedisLettuceProperties setNodes(String nodes) {
-        this.nodes = nodes;
-        return this;
-    }
-
-    public int getMaxRedirects() {
-        return maxRedirects;
-    }
-
-    public RedisLettuceProperties setMaxRedirects(int maxRedirects) {
-        this.maxRedirects = maxRedirects;
-        return this;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public RedisLettuceProperties setPassword(String password) {
-        this.password = password;
-        return this;
-    }
-
-    public boolean isLoadRemoteCachesOnStartup() {
-        return loadRemoteCachesOnStartup;
-    }
-
-    public RedisLettuceProperties setLoadRemoteCachesOnStartup(boolean loadRemoteCachesOnStartup) {
-        this.loadRemoteCachesOnStartup = loadRemoteCachesOnStartup;
-        return this;
-    }
-
-    public boolean isUsePrefix() {
-        return usePrefix;
-    }
-
-    public RedisLettuceProperties setUsePrefix(boolean usePrefix) {
-        this.usePrefix = usePrefix;
-        return this;
-    }
-
-    public int getDefaultExpiration() {
-        return defaultExpiration;
-    }
-
-    public RedisLettuceProperties setDefaultExpiration(int defaultExpiration) {
-        this.defaultExpiration = defaultExpiration;
-        return this;
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
     }
 }

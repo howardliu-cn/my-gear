d866463679343826b9519327816c201d110dbb73
howardliu-cn <liuxinghao1988@gmail.com>
2018-03-28 14:28:13 +0800

    FileUpload tester

--- gear-httpclient/src/test/java/cn/howardliu/gear/commons/httpclient/FileUploadTester.java
+++ gear-httpclient/src/test/java/cn/howardliu/gear/commons/httpclient/FileUploadTester.java
@@ -1,6 +1,5 @@
 package cn.howardliu.gear.commons.httpclient;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
@@ -10,6 +9,7 @@ import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,18 +27,21 @@ public class FileUploadTester {
     private static final Logger logger = LoggerFactory.getLogger(FileUploadTester.class);
 
     @Test
+    @Ignore
     public void test() throws IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost("http://localhost:8080/spring-mvc-jsonp/file/upload");
-        HttpEntity httpEntity = MultipartEntityBuilder.create()
-                .addPart("username", new StringBody("a", ContentType.TEXT_PLAIN))
-                .addPart("nickname", new StringBody("b", ContentType.TEXT_PLAIN))
-                .addPart("files", new FileBody(new File("/home/liuxh/Pictures/favicon.png")))
-                .addPart("files", new FileBody(new File("/home/liuxh/Pictures/Coder.png")))
-                .build();
-        httpPost.setEntity(httpEntity);
+        httpPost.setEntity(
+                MultipartEntityBuilder.create()
+                        .addPart("username", new StringBody("a", ContentType.TEXT_PLAIN))
+                        .addPart("nickname", new StringBody("b", ContentType.TEXT_PLAIN))
+                        .addPart("files", new FileBody(new File("/home/liuxh/Pictures/favicon.png")))
+                        .addPart("files", new FileBody(new File("/home/liuxh/Pictures/Coder.png")))
+                        .build()
+        );
         CloseableHttpResponse response = httpClient.execute(httpPost);
         System.out.println(EntityUtils.toString(response.getEntity()));
         response.close();
+        httpClient.close();
     }
 }

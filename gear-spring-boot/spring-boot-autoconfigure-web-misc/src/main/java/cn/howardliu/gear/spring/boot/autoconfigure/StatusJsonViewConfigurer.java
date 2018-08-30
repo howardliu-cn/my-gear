dc644fece6bc394f18035cebe2678285b43c9d6b
howardliu-cn <liuxinghao1988@gmail.com>
2018-08-30 14:39:06 +0800

    use WebMvcConfigurer

--- gear-spring-boot/spring-boot-autoconfigure-web-misc/src/main/java/cn/howardliu/gear/spring/boot/autoconfigure/StatusJsonViewConfigurer.java
+++ gear-spring-boot/spring-boot-autoconfigure-web-misc/src/main/java/cn/howardliu/gear/spring/boot/autoconfigure/StatusJsonViewConfigurer.java
@@ -1,14 +1,12 @@
 package cn.howardliu.gear.spring.boot.autoconfigure;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * <br>created at 17-5-18
@@ -20,13 +18,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @ConditionalOnProperty(name = "spring.mvc.index.page", havingValue = "status.json")
 @AutoConfigureAfter(DispatcherServletAutoConfiguration.class)
-public class StatusJsonViewConfigurer extends WebMvcConfigurerAdapter {
-    private static final Logger logger = LoggerFactory.getLogger(StatusJsonViewConfigurer.class);
-
+public class StatusJsonViewConfigurer implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("redirect:/status.json");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        super.addViewControllers(registry);
     }
 }

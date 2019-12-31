7df6fc0777e225e5933f7806bc0c9351bb98b6f2
howardliu-cn <liuxinghao1988@gmail.com>
2019-12-31 13:01:04 +0800

    add NetUtils

---
+++ gear-commons/src/main/java/cn/howardliu/gear/commons/utils/NetUtils.java
@@ -0,0 +1,65 @@
+package cn.howardliu.gear.commons.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * <br>created at 2019/10/31
+ *
+ * @author liuxh
+ * @since 1.0.0
+ */
+public class NetUtils {
+    private static final Logger logger = LoggerFactory.getLogger(NetUtils.class);
+
+    public static String getIpAddress(HttpServletRequest request) {
+        // 获取请求主机IP地址,如果通过代理进来，则透过防火墙获取真实IP地址
+        String ip = request.getHeader("X-Forwarded-For");
+        if (logger.isInfoEnabled()) {
+            logger.info("getIpAddress(HttpServletRequest) - X-Forwarded-For - String ip=" + ip);
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("Proxy-Client-IP");
+                if (logger.isInfoEnabled()) {
+                    logger.info("getIpAddress(HttpServletRequest) - Proxy-Client-IP - String ip=" + ip);
+                }
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("WL-Proxy-Client-IP");
+                if (logger.isInfoEnabled()) {
+                    logger.info("getIpAddress(HttpServletRequest) - WL-Proxy-Client-IP - String ip=" + ip);
+                }
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_CLIENT_IP");
+                if (logger.isInfoEnabled()) {
+                    logger.info("getIpAddress(HttpServletRequest) - HTTP_CLIENT_IP - String ip=" + ip);
+                }
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+                if (logger.isInfoEnabled()) {
+                    logger.info("getIpAddress(HttpServletRequest) - HTTP_X_FORWARDED_FOR - String ip=" + ip);
+                }
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getRemoteAddr();
+                if (logger.isInfoEnabled()) {
+                    logger.info("getIpAddress(HttpServletRequest) - getRemoteAddr - String ip=" + ip);
+                }
+            }
+        } else if (ip.length() > 15) {
+            String[] ips = ip.split(",");
+            for (String ip1 : ips) {
+                if (!("unknown".equalsIgnoreCase(ip1))) {
+                    ip = ip1;
+                    break;
+                }
+            }
+        }
+        return ip;
+    }
+}
